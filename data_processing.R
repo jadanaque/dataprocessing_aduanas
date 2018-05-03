add5f7ec63d6a0930537c8a86c0e4b752a50aa2b
jadanaque <javieradanaque@gmail.com>
2018-05-03 13:04:50 -0500

    Data reading

--- data_processing.R
+++ data_processing.R
@@ -5,3 +5,9 @@ library(tibble)
 library(tidyr)
 library(readxl)
 library(openxlsx)
+
+# Reading data
+aduanas_df <- read_excel("data/2016-2017.xlsx", sheet = 1, col_types = "text")
+
+competition_data <- map_dfr(excel_sheets("data/Envio Barbi competidores final.xlsx"), ~read_excel("data/Envio Barbi competidores final.xlsx", sheet = ., range = cell_cols("B:H"), col_types = "text"))  # Data given from: Wella, Revlon-AV & Schwarzkoft
+
