0b22944a269ba694a5496e3e4c75cc555bbdfddf
jadanaque <javieradanaque@gmail.com>
2018-05-03 13:47:43 -0500

    fixes column names

--- data_processing.R
+++ data_processing.R
@@ -11,3 +11,12 @@ aduanas_df <- read_excel("data/2016-2017.xlsx", sheet = 1, col_types = "text")
 
 competition_data <- map_dfr(excel_sheets("data/Envio Barbi competidores final.xlsx"), ~read_excel("data/Envio Barbi competidores final.xlsx", sheet = ., range = cell_cols("B:H"), col_types = "text"))  # Data given from: Wella, Revlon-AV & Schwarzkoft
 
+names(competition_data) <- names(competition_data) %>%
+  make.names() %>%
+  iconv(to="ASCII//TRANSLIT") %>%
+  str_replace_all("[^[:alnum:]]", "")
+
+names(aduanas_df) <- names(aduanas_df) %>%
+  make.names() %>%
+  iconv(to = "ASCII//TRANSLIT") %>%
+  str_replace_all("[^[:alnum:]]", "")
